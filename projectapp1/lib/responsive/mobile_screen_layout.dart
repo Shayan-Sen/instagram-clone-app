6490b148244567572a61b00ec429c793e4710d69
Shayan Sen <senshayan2103@gmail.com>
2024-07-07 23:29:26 +0530

    Made a half working bottom app bar and I am frustrated so I will call it a day

--- projectapp1/lib/responsive/mobile_screen_layout.dart
+++ projectapp1/lib/responsive/mobile_screen_layout.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:projectapp1/utils/colors.dart';
 
 class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({super.key});
@@ -8,10 +10,41 @@ class MobileScreenLayout extends StatefulWidget {
 }
 
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  int x = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("user.email")),
+      body: Center(child: Text("This is mobile")),
+      bottomNavigationBar: CupertinoTabBar(
+        activeColor: Colors.white,
+        backgroundColor: mobileBackgroundColor,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home",
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: "Search",
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_box_outlined),
+              label: "Add",
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: "Notifications",
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: "Profile",
+              backgroundColor: primaryColor)
+        ],
+        currentIndex: x,
+        onTap: (value) {
+          x = value;
+        },
+      ),
     );
   }
 }
